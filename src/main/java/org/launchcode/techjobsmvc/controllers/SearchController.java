78315a70d5994c02f4c8f934c913614e59f9054d
Brittney Epperson <beppers87@hotmail.com>
2023-10-12 09:11:11 -0500

    Finished Bonus 1

--- src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobsmvc/controllers/SearchController.java
@@ -2,12 +2,10 @@ package org.launchcode.techjobsmvc.controllers;
 
 import org.launchcode.techjobsmvc.models.Job;
 import org.launchcode.techjobsmvc.models.JobData;
+import org.launchcode.techjobsmvc.models.Search;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 
@@ -24,12 +22,13 @@ public class SearchController {
     @GetMapping(value = "")
     public String search(Model model) {
         model.addAttribute("columns", columnChoices);
+        model.addAttribute("search", new Search());
         return "search";
     }
 
     // Task #3 - Create a handler to process a search request and render the updated search view.
     @PostMapping(value = "results")
-    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
+    public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm, @ModelAttribute Search search) {
         ArrayList<Job> jobs = new ArrayList<>();
         if (searchTerm.equals("all") || searchTerm.equals("") || searchTerm == null) {
             jobs = JobData.findAll();
---
+++ src/main/java/org/launchcode/techjobsmvc/models/Search.java
@@ -0,0 +1,15 @@
+package org.launchcode.techjobsmvc.models;
+
+public class Search {
+
+    private String searchType;
+
+    public String getSearchType() {
+        return searchType;
+    }
+
+    public void setSearchType(String searchType) {
+        this.searchType = searchType;
+    }
+
+}
